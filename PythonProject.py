5490de0c905275467bbe467dc333ebdcc48b6347
j-as-o-n <jrvn@protonmail.com>
2019-04-22 13:58:18 -0700

    Added buttons (interact, reset)

--- PythonProject.py
+++ PythonProject.py
@@ -12,7 +12,13 @@ from tkinter import *
 
 #def go_back():
 
+#def reset():
 
+#def open_door():
+
+#def talk():
+
+#def read_note():
 #############################################################
 
 #Frames
@@ -22,7 +28,7 @@ root.geometry("450x550")
 root.configure(bg = "black")
 
 
-move_butt_frame = Frame(root, bg = "black")
+move_butt_frame = Frame(root)
 move_butt_frame.pack(side = BOTTOM)
 
 ##############################################################
@@ -30,10 +36,10 @@ move_butt_frame.pack(side = BOTTOM)
 
 #Creating and placing the 'movement' buttons
 left_button = Button(move_butt_frame, text = "<", width = 5, height = 2, bg = "blue", fg = "white")
-left_button.grid(row = 0, column = 0, rowspan = 2) 
+left_button.grid(row = 0, column = 0) 
 
 right_button = Button(move_butt_frame, text = ">", width = 5, height = 2, bg = "blue", fg = "white")
-right_button.grid(row = 0, column = 2, rowspan = 2)
+right_button.grid(row = 0, column = 2)
 
 forward_button = Button(move_butt_frame, text = "^", width = 5, height = 2, bg = "blue", fg = "white")
 forward_button.grid(row = 0, column = 1)
@@ -41,7 +47,11 @@ forward_button.grid(row = 0, column = 1)
 backward_button = Button(move_butt_frame, text = "v", width = 5, height = 2, bg = "blue", fg = "white")
 backward_button.grid(row = 1, column = 1)
 
+interact_button = Button(move_butt_frame, text = "Interact", width = 5, height = 2, bg = "green", fg = "white") #command = lambda : open_door(), talk(), read_note()
+interact_button.grid(row = 1, column = 2)
 
+reset_button = Button(move_butt_frame, text = "RESET", width = 5, height = 2, bg = "red", fg = "white")
+reset_button.grid(row = 1, column = 0)
 
 
 
