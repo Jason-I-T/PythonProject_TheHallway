5fe076e2dcf3cbe3771210d17d6305b03ff46896
Huatuo <49883254+1523279006@users.noreply.github.com>
2019-05-01 09:12:21 -0700

    Add files via upload

---
+++ Background Story Code.py
@@ -0,0 +1,17 @@
+import tkinter
+
+def doButton():
+
+ label_1.configure(text = "Background Story 2")
+
+xd = tkinter.Tk()
+
+label_1 = tkinter.Label(xd, text="Background Story 1")
+
+label_1.pack()
+
+button_1 = tkinter.Button(xd, text="Next", command=doButton)
+
+button_1.pack()
+
+xd.mainloop()
