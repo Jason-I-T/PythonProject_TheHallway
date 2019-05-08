59f9e0801105d51d4613be17551fbeee21eae6cf
Huatuo <49883254+1523279006@users.noreply.github.com>
2019-05-07 22:22:03 -0700

    Add files via upload

---
+++ python idle1.py
@@ -0,0 +1,11 @@
+import tkinter
+import tkinter.messagebox
+
+root = tkinter.Tk()
+
+tkinter.messagebox.showinfo("Instruction", "Press interact to start.")
+
+tkinter.messagebox.showinfo("Instruction", "You die, press reset to reset")
+
+tkinter.messagebox.showinfo("Instruction", "Text")
+root.mainloop()
